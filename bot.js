e870af58cd29ad22215edec594d43e198d216920
woprkwqpmr <44927541+woprkwqpmr@users.noreply.github.com>
2018-11-23 20:53:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
-bot.on('guildMemberAdd', member => {
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("512388317925343242");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(` Welcome to !legends united :revolving_hearts: ${member}`), 4000)        
 }
-})
+});
+client.login(process.env.BOT_TOKEN);
